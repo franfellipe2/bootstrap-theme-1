34dcd4e04bc9978fe158f210e8fe3e3d0ac83b56
sie <franfellipe2@hotmail.com>
2017-10-13 19:48:46 -0300

    site-introduction

--- index.php
+++ index.php
@@ -9,39 +9,63 @@
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
         <!-- font-awesome CSS --> 
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <!-- boot CSS --> 
+        <link href="css/boot.css" rel="stylesheet" type="text/css"/>
+        <!-- styles pesrsonalizados CSS --> 
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <div class="container">
-
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#!">bootstrap 4 theme1</a>
+        <div class="site-introduction">
                 
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul class="navbar-nav mr-auto mt-2 mt-md-0">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#!">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#!">Link</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+                <header class="site-header fixed-top">
+                    <h1 class="screen-reader-text">Curso bootstrap 4</h1>
+                    <div class="container">
 
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <h1 class="screen-reader-text">Menu Principal</h1>
+                            <a class="navbar-brand" href="#!">bootstrap 4 theme1</a>
 
-        </div>
+                            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#mainMenu" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                                <i class="fa fa-bars" aria-hidden="true"></i>
+                            </button>
 
+                            <div class="collapse navbar-collapse" id="mainMenu">
+                                <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="#!">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#!">Sobre Nós</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#!">Serviços</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#!">Protfólio</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#!">Contato</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </header>
+                <article class="container">
+                    <div class="site-introduction-content">
+                        <h1>Floral Libert</h1>
+                        <p>Exclusivo para selebrar a nova estção do Verão</p>
+                        <a href="#" class="btn btn-dark">Ver Agora!</a>
+                    </div>
+                </article>
 
-
+        </div><!-- /.site-introduction -->
 
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="js/jquery.js" type="text/javascript"></script>
+        <script src="js/popper.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
     </body>  
 </html>
